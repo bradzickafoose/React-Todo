d2532452f27c259787d674f25f3a75dd58e6f624
Brad <brad@viziworx.com>
2019-10-28 16:11:18 -0400

    Setup constructor with state. Added addTask class method to update state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
 import React from 'react';
 
+const data = [
+	{
+		task      : 'Organize Garage',
+		id        : 1528817077286,
+		completed : false,
+	},
+	{
+		task      : 'Bake Cookies',
+		id        : 1528817084358,
+		completed : false,
+	},
+];
+
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
-    return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
-    );
-  }
+	// you will need a place to store your state in this component.
+	// design `App` to be the parent component of your application.
+	// this component is going to take care of state, and any change handlers you need to work with your state
+	constructor() {
+		super();
+		this.state = {
+			task : data,
+		};
+	}
+	// Class methods to update state
+	addTask = (task) => {
+		// update task state with a new task
+		const newTask = {
+			task      : task,
+			id        : Date.now(),
+			completed : false,
+		};
+		this.setState({
+			task : [ ...this.state.task, newTask ],
+		});
+	};
+
+	render() {
+		return (
+			<div>
+				<h2>Welcome to your Todo App!</h2>
+			</div>
+		);
+	}
 }
 
 export default App;
